03e4a4ae8ea87bf9ca3794ce266edc1c3590394f
Claire Campbell <cc@springlaod.co.nz>
2018-03-15 16:41:25 +1300

    adding dummy form to check information displays correctly

--- src/js/components/Form/Form.js
+++ src/js/components/Form/Form.js
@@ -156,9 +156,129 @@ export default function(props) {
                     onChange={props.handleChange}
                 />
             </div>
+            { props.values["diet"] === "yes" &&
+                <div className="form__group">
+                    <p>Who has dietry requirements</p>
+                    <FormField 
+                        labelName="John Doe" 
+                        type="checkbox" 
+                        id="diet-multi-guest-1" 
+                        value="john-doe"
+                        name="diet-multi.0"
+                        checked={props.values["diet-multi"] && props.values["diet-multi"][0]}
+                        onChange={props.handleChange}
+                    />
+                    { props.values["diet-multi"] && props.values["diet-multi"][0] &&
+                        <FormField 
+                            labelName="Please enter requirement" 
+                            type="text" 
+                            id="johnDoeDiet" 
+                            onChange={props.handleChange}
+                            value={props.values.johnDoeDiet} 
+                            name="johnDoeDiet"
+                        />
+                    }
+                    <FormField 
+                        labelName="Jane Doe" 
+                        type="checkbox" 
+                        id="diet-multi-guest-2" 
+                        value="jane-doe" 
+                        name="diet-multi.1"
+                        checked={props.values["diet-multi"] && props.values["diet-multi"][1]}
+                        onChange={props.handleChange}
+                    />
+                    { props.values["diet-multi"] && props.values["diet-multi"][1] &&
+                        <FormField 
+                            labelName="Please enter requirement" 
+                            type="text" 
+                            id="janeDoeDiet" 
+                            onChange={props.handleChange}
+                            value={props.values.janeDoeDiet} 
+                            name="janeDoeDiet"
+                        />
+                    }
+                    <FormField 
+                        labelName="Jack Doe" 
+                        type="checkbox" 
+                        id="diet-multi-guest-3" 
+                        value="jack-doe"
+                        name="diet-multi.2"
+                        checked={props.values["diet-multi"] && props.values["diet-multi"][2]}
+                        onChange={props.handleChange}
+                    />
+                    { props.values["diet-multi"] && props.values["diet-multi"][2] &&
+                        <FormField 
+                            labelName="Please enter requirement" 
+                            type="text" 
+                            id="jackDoeDiet" 
+                            onChange={props.handleChange}
+                            value={props.values.jackDoeDiet} 
+                            name="jackDoeDiet"
+                        />
+                    }
+                    <FormField 
+                        labelName="Janie Doe" 
+                        type="checkbox" 
+                        id="diet-multi-guest-4" 
+                        value="janie-doe" 
+                        name="diet-multi.3"
+                        checked={props.values["diet-multi"] && props.values["diet-multi"][3]}
+                        onChange={props.handleChange}
+                    />
+                    { props.values["diet-multi"] && props.values["diet-multi"][3] &&
+                        <FormField 
+                            labelName="Please enter requirement" 
+                            type="text" 
+                            id="janieDoeDiet" 
+                            onChange={props.handleChange}
+                            value={props.values.janieDoeDiet} 
+                            name="janieDoeDiet"
+                        />
+                    }
+                </div>
+            }
 
             <div className="form__group">
-                <p>Who is the best contact for your group if we need to update you with any information?</p>
+                <p>Please choose a group leader we can contact if we need to</p>
+                <FormField 
+                    labelName="John Doe" 
+                    type="radio" 
+                    id="leader-guest-1" 
+                    value="leader-john"
+                    name="nominated-leader"
+                    checked={props.values["nominated-leader"] === "leader-john"}
+                    onChange={props.handleChange}
+                />
+                <FormField 
+                    labelName="Jane Doe" 
+                    type="radio" 
+                    id="leader-guest-2" 
+                    value="leader-jane" 
+                    name="nominated-leader"
+                    checked={props.values["nominated-leader"] === "leader-jane"}
+                    onChange={props.handleChange}
+                />
+                <FormField 
+                    labelName="Jack Doe" 
+                    type="radio" 
+                    id="leader-guest-3" 
+                    value="leader-jack"
+                    name="nominated-leader"
+                    checked={props.values["nominated-leader"] === "leader-jack"}
+                    onChange={props.handleChange}
+                />
+                <FormField 
+                    labelName="Janie Doe" 
+                    type="radio" 
+                    id="leader-guest-4" 
+                    value="leader-janie" 
+                    name="nominated-leader"
+                    checked={props.values["nominated-leader"] === "leader-janie"}
+                    onChange={props.handleChange}
+                />
+            </div>
+            <div className="form__group">
+                <p>Please provide contact details for that person</p>
                 <FormField 
                     labelName="Address" 
                     type="text" 
@@ -183,10 +303,6 @@ export default function(props) {
                     value={props.values.email} 
                     name="email"
                 />
-                <ul>
-                    <li></li>
-                    <li className="class"></li>
-                </ul>
             </div>
             <div>
                 <button type="submit">Submit</button>
