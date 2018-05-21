5492d12635bbaa89c6e84a9d08316b49d52f59e9
Claire Campbell <cc@springlaod.co.nz>
2018-05-21 13:17:12 +1200

    Changing to stateful component to allow loaded state for header animation

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -1,27 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Image from '../Image/Image';
+import classNames from 'classnames';
 import './header.css';
 
-export default function() {
-    return (
-        <div className="header">
-            <div className="header__content">
-                <h1 className="header__title">Jack &amp; Jill</h1>
-                <p className="header__intro">
-                    <span className="header__intro-item">April 6. 2019</span>
-                    <span className="header__intro-item">
-                        Wellington, New Zealand
-                    </span>
-                </p>
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ loaded: true });
+        }, 0);
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <div className="header__content">
+                    <h1
+                        className={classNames('header__title', {
+                            'header__title--loaded': this.state.loaded,
+                        })}
+                    >
+                        Jack &amp; Jill
+                    </h1>
+                    <p
+                        className={classNames('header__intro', {
+                            'header__intro--loaded': this.state.loaded,
+                        })}
+                    >
+                        <span className="header__intro-item">
+                            April 6. 2019
+                        </span>
+                        <span className="header__intro-item">
+                            Wellington, New Zealand
+                        </span>
+                    </p>
+                </div>
+                <div className="header__image">
+                    <Image
+                        className="image"
+                        src="/assets/images/flower-banner.jpg"
+                        alt="flower banner"
+                        height="900px"
+                    />
+                </div>
             </div>
-            <div className="header__image">
-                <Image
-                    className="image"
-                    src="/assets/images/flower-banner.jpg"
-                    alt="flower banner"
-                    height="900px"
-                />
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Header;
