6c12500ebe0c1c36def29720f7fb1b539df7cd92
Claire Campbell <cece@Claires-MacBook-Pro.local>
2018-07-14 08:27:57 +1200

    moved the header to a ladning component

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import Content from './components/Content/Content';
 import Dashboard from './components/Dashboard/Dashboard';
 import Footer from './components/Footer/Footer';
@@ -10,10 +10,10 @@ import '../../src/App.css';
 export default function() {
     return (
         <div className="app">
-            <Header />
+            {/* <Header /> */}
             <Route path="/guests/:name?" component={Content} />
             <Route path="/foobar" component={Dashboard} />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 }
--- src/js/components/Content/Landing/Landing.js
+++ src/js/components/Content/Landing/Landing.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Image from '../Image/Image';
 import classNames from 'classnames';
-import './header.css';
+import './landing.css';
 
-class Header extends Component {
+class Landing extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -19,11 +19,11 @@ class Header extends Component {
 
     render() {
         return (
-            <div className="header">
-                <div className="header__content">
+            <div className="landing">
+                <div className="landing__content">
                     <h1
-                        className={classNames('header__title', {
-                            'header__title--loaded': this.state.loaded,
+                        className={classNames('landing__title', {
+                            'landing__title--loaded': this.state.loaded,
                         })}
                     >
                         <Image
@@ -34,17 +34,19 @@ class Header extends Component {
                         />
                     </h1>
                     <h3
-                        className={classNames('header__intro', {
-                            'header__intro--loaded': this.state.loaded,
+                        className={classNames('landing__intro', {
+                            'landing__intro--loaded': this.state.loaded,
                         })}
                     >
-                        <span className="header__intro-item">April 6 2019</span>
-                        <span className="header__intro-item">
+                        <span className="landing__intro-item">
+                            April 6 2019
+                        </span>
+                        <span className="landing__intro-item">
                             Wellington NZ
                         </span>
                     </h3>
                 </div>
-                <div className="header__image">
+                <div className="landing__image">
                     {/*<Image
                         className="image"
                         src="/assets/images/ccmatt.jpg"
@@ -57,4 +59,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default Landing;
